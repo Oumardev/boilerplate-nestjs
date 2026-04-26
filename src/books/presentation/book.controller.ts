255ff130a822921e045c5c9bf6d2d3eda3d70873
Oumar <oumar@192.168.1.2>
2026-04-26 13:33:43 +0000

    Fix pagination

--- src/books/presentation/book.controller.ts
+++ src/books/presentation/book.controller.ts
@@ -42,8 +42,8 @@ export class BookController {
 
     @Get()
     @HttpCode(200)
-    async findAll(@Query('page') page: number = 1, @Query('limit') limit: number = 10) {
-        const query = new FindBooksQuery(page, limit);
+    async findAll(@Query('page') limit: number = 10, @Query('limit') offset: number = 0) {
+        const query = new FindBooksQuery(limit, offset);
 
         return this.queryBus.execute(query);
     }
